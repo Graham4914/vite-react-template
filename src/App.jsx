6fe7f818a5dfcbd6b5a772115965e82c3a89924c
Graham4914 <67054735+Graham4914@users.noreply.github.com>
2024-12-30 18:39:13 +0800

    Initial commit for Vite React template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+
+  const App = () => {
+    return (
+      <div>
+        <h1>Welcome to Your Project</h1>
+      </div>
+    );
+  };
+  
+  export default App;
+  
